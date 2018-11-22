608466344b4590826766ece3f0bcc75cce1e4ea5
Marcelo Zabani <mzabani@gmail.com>
2018-11-22 21:45:19 -0200

    - Integração com Facebook começa a funcionar - Começo de página de cadastro independente

--- src/Main.hs
+++ src/Main.hs
@@ -4,11 +4,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DuplicateRecordFields #-}
 module Main where
 
 import Data.Text
 import Servant
 import Servant.API
+import Servant.HTML.Blaze
+import ServantExtensions
 import Network.Wai
 import Network.Wai.Handler.Warp
 import GHC.Generics
@@ -16,39 +20,49 @@ import Data.Aeson
 import Web.FormUrlEncoded
 import Database.PostgreSQL.Simple
 import Data.Pool
+import Data.Maybe
 import Control.Monad.IO.Class
 import Database.Beam
 import Database.Beam.Postgres
+import Facebook
 import Text.Blaze.Html
-import Servant.HTML.Blaze
+import Text.Hamlet
+import Network.HTTP.Client hiding (Proxy)
+import Network.HTTP.Client.TLS
 import Model.Aprovados
-import Heist
-import Heist.Interpreted
 import Control.Lens
 import qualified Data.ByteString      as BS
 import qualified Data.ByteString.Lazy as LBS
 import Data.Text.Lazy.Encoding
 import Data.Binary.Builder
 import Data.String.Conv
+import Control.Monad.Trans.Resource
+import Control.Monad.IO.Unlift
+import Control.Exception.Safe hiding (Handler)
 
-data DadosCadastro = DadosCadastro { nomeCompleto :: Text, email :: Text } deriving Generic
+data DadosCadastro = DadosCadastro { cadastroNomeCompleto :: Text, cadastroEmail :: Text } deriving Generic
+data DadosFacebookLogin = DadosFacebookLogin { accessToken :: Text } deriving Generic
+data DadosPrePreenchidosParaCadastro = DadosPrePreenchidosParaCadastro { nomeCompleto :: Text, email :: Text, accessToken :: Text } deriving Generic
 
 -- instance FromJSON DadosCadastro
 instance FromForm DadosCadastro
---instance ToJSON DadosCadastro
+instance FromForm DadosFacebookLogin
+instance ToJSON DadosPrePreenchidosParaCadastro
 
-type UserAPI = "cadastrar" :> ReqBody '[FormUrlEncoded] DadosCadastro :> Post '[JSON] Bool
+type UserAPI = "cadastro" :> ReqBody '[FormUrlEncoded] DadosCadastro :> PostRedirect 301 String
+                :<|> "cadastro" :> Get '[HTML] Html
                 :<|> Get '[HTML] Html
+                :<|> "fbLogin" :> ReqBody '[FormUrlEncoded] DadosFacebookLogin :> Post '[JSON] DadosPrePreenchidosParaCadastro
                 :<|> Raw
 
 userAPI :: Proxy UserAPI
 userAPI = Proxy
 
-server :: Pool Connection -> HeistState Handler -> Server UserAPI
-server connectionPool heistState = cadastrar connectionPool :<|> Main.index heistState :<|> serveDirectoryWebApp "html"
+server :: Pool Connection -> Server UserAPI
+server connectionPool = cadastroPost connectionPool :<|> cadastroGet :<|> Main.index :<|> Main.fbLogin :<|> serveDirectoryWebApp "html"
 
-app :: Pool Connection -> HeistState Handler -> Application
-app connectionPool heistState = serve userAPI (server connectionPool heistState)
+app :: Pool Connection -> Application
+app connectionPool = serve userAPI (server connectionPool)
 
 -- Ideias importantes:
 -- Deixar usuários livres para visualizarem os últimos 7 diários de cada tipo (garantir que não acessem diários anteriores a isso e que as URLs sejam baseadas em hashes para evitar que alguém nos copie com facilidade)
@@ -57,51 +71,56 @@ app connectionPool heistState = serve userAPI (server connectionPool heistState)
 main :: IO ()
 main = do
     connectionPool <- createPool (connect connString) close 1 300 10
-    heistState <- billy
     -- TODO: chamar "run" dentro de "bracket"
-    run 8080 (app connectionPool heistState)
+    run 8080 (app connectionPool)
     destroyAllResources connectionPool
 
-billy :: IO (HeistState Handler)
-billy = do
-    heist <- initHeist $ emptyHeistConfig
-        & hcTemplateLocations .~ [ loadTemplates "templates" ]
-        & hcInterpretedSplices .~ defaultInterpretedSplices
-        & hcNamespace .~ ""
-    case heist of
-        Left errs -> error $ Prelude.foldl (++) "Error on initHeist: " errs
-        Right heistState -> do
-            return heistState
-            -- Just (output, _) <- renderTemplate heistState "index"
-            -- liftIO . BS.putStrLn . toLazyByteString $ output
+cadastroGet :: Handler Html
+cadastroGet = return (toHtml $(shamletFile "templates/cadastro.hamlet"))
 
-cadastrar :: Pool Connection -> DadosCadastro -> Handler Bool
-cadastrar connPool dc =
-    liftIO $ withResource connPool $ \conn -> withTransaction conn $ do
+cadastroPost :: Pool Connection -> DadosCadastro -> RedirectHandler String -- Handler String
+cadastroPost connPool dc = do
+    liftIO $ withResource connPool $ \conn -> withTransaction conn $
         runBeamPostgresDebug putStrLn conn $ runInsert $ insert (_usuarios aprovadosDb) $
             insertExpressions [UsuarioT {
                 _usuarioId = default_
-                , _usuarioEmail = val_ (email dc)
-                , _usuarioNomeCompleto = val_ (nomeCompleto dc)
+                , _usuarioEmail = val_ (cadastroEmail dc)
+                , _usuarioNomeCompleto = val_ (cadastroNomeCompleto dc)
             }]
-        return True
+    redirect "/cadastro-realizado"
 
-renderBlazeHtmlOrError :: HeistState Handler -> BS.ByteString -> Handler Html
-renderBlazeHtmlOrError heistState templateName = do
-    outputAndMime <- renderTemplate heistState templateName
-    case outputAndMime of
-        Nothing -> error $ "Could not render template " ++ toS templateName
-        Just (binaryBuilder, mimeType) ->
-            case mimeType of
-                "text/html;charset=utf-8" -> do
-                    let htmlMarkup = unsafeLazyByteString . toLazyByteString $ binaryBuilder
-                    -- liftIO $ print htmlMarkup
-                    return $ toHtml htmlMarkup
-                _           -> error $ "Template " ++ toS templateName ++ " is of non-html mime type " ++ toS mimeType
+index :: Handler Html
+index = let name = ("Marcelo" :: String) in return (toHtml $(shamletFile "templates/index.hamlet"))
 
-index :: HeistState Handler -> Handler Html
--- index = return (toHtml $(shamletFile "html/index.html"))
-index heistState = renderBlazeHtmlOrError heistState "index"
+fbAppToken :: FacebookT Auth (ResourceT IO) AppAccessToken
+fbAppToken = do
+    -- TODO: Cachear o AppAccessToken por tempo
+    accToken <- getAppAccessToken
+    return accToken
+
+getFbUserFromAccessToken :: MonadIO m => Manager -> Text -> m (Maybe User)
+getFbUserFromAccessToken mgr accessToken = liftIO . runResourceT $ runFacebookT Credentials {
+    appName = "ePassei"
+    , appId = "336251103621017"
+    , appSecret = "28f29b85e3aed5501496e2e0acc7f183"
+    } mgr $ do
+        appAccToken <- fbAppToken
+        dt <- debugToken appAccToken accessToken
+        case (dtIsValid dt, dtScopes dt, dtAccessToken dt, dtUserId dt) of
+            (Just True, Just perms, Just usrAccToken, Just userID) -> Just <$> getUser userID [] (Just usrAccToken)
+            _ -> return Nothing
+
+fbLogin :: DadosFacebookLogin -> Handler DadosPrePreenchidosParaCadastro
+fbLogin DadosFacebookLogin{..} = do
+    mgr <- newTlsManagerWith tlsManagerSettings
+    fbUsrMaybe <- getFbUserFromAccessToken mgr accessToken
+    case fbUsrMaybe of
+        Nothing -> error "Oops!"
+        Just fbUsr -> return DadosPrePreenchidosParaCadastro {
+            nomeCompleto = fromMaybe "" (userName fbUsr)
+            , email = fromMaybe "" (userEmail fbUsr)
+            , accessToken = accessToken
+        }
 
 connString :: ConnectInfo
 connString = defaultConnectInfo { connectHost = "localhost", connectUser = "mzabani", connectDatabase = "epassei" }
---
+++ src/ServantExtensions.hs
@@ -0,0 +1,38 @@
+{- Taken from gist https://gist.github.com/alpmestan/757094ecf9401f85c5ba367ca20b8900, found at https://stackoverflow.com/questions/46789241/redirections-in-servant -}
+{-
+$ curl -X POST localhost:9876/dog -v
+* Connected to localhost (127.0.0.1) port 9876 (#0)
+> POST /dog HTTP/1.1
+> Host: localhost:9876
+> User-Agent: curl/7.55.1
+> Accept: */*
+> 
+< HTTP/1.1 301 Moved Permanently
+< Transfer-Encoding: chunked
+< Date: Tue, 17 Oct 2017 12:08:01 GMT
+< Server: Warp/3.2.13
+< Content-Type: application/json;charset=utf-8
+< Location: https://google.com/?q=dog
+< 
+* Connection #0 to host localhost left intact
+-}
+
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeOperators #-}
+module ServantExtensions where
+
+import GHC.TypeLits
+import Network.Wai.Handler.Warp
+import Servant
+
+type PostRedirect (code :: Nat) loc
+  = Verb 'POST code '[JSON] (Headers '[Header "Location" loc] NoContent)
+
+type RedirectHandler loc = Handler (Headers '[Header "Location" loc] NoContent)
+
+redirect
+  :: ToHttpApiData loc
+  => loc -- ^ what to put in the 'Location' header
+  -> RedirectHandler loc
+redirect a = return (addHeader a NoContent)
