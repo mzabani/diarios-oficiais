ccf4e6002c0782ab137cde510571f0aa8d6d8641
Marcelo Zabani <mzabani@gmail.com>
2019-09-02 22:09:28 -0300

    Pequenos fixes no controle de versão do DB

--- scripts/aplicar-migracao.sh
+++ scripts/aplicar-migracao.sh
@@ -2,11 +2,11 @@
 
 set -e
 
+NOME="$(basename $1)"
 HASH_MIGRACAO="$(sha512sum $1 | cut -f 1 -d ' ')"
-
-JA_APLICADA=$(echo "SELECT 1 FROM migracoes_sql WHERE hash_arquivo='${HASH_MIGRACAO}';" | psql -U postgres -qt $PGDATABASE)
+JA_APLICADA=$(echo "SELECT 1 FROM migracoes_sql WHERE nome_arquio='${NOME}';" | psql -U postgres -qt $PGDATABASE)
 if [ -z "$JA_APLICADA" ]; then
-    (echo "BEGIN;"; cat $1; echo "INSERT INTO migracoes_sql (nome_arquivo, hash_arquivo) VALUES ('$1', '${HASH_MIGRACAO}'); COMMIT;") | psql -U postgres $PGDATABASE
+    (echo "BEGIN;"; cat $1; echo "INSERT INTO migracoes_sql (nome_arquivo, hash_arquivo) VALUES ('$NOME', '${HASH_MIGRACAO}'); COMMIT;") | psql -U postgres $PGDATABASE
 else
     echo "------------------- Migraçao $1 já aplicada"
 fi;
