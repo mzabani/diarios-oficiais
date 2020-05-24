3338dc44e51307ffcd6f45dc2eebb8091a39cf34
Marcelo Zabani <mzabani@gmail.com>
2020-05-24 13:46:45 -0300

    certbot certs em docker volume agora

--- src/backend/src/Ler.hs
+++ src/backend/src/Ler.hs
@@ -16,3 +16,15 @@ lerDiario connPool conteudoDiarioId =
         case res of
             [] -> throwError err404
             _ -> return $ forM_ res $ (\(Only (t :: T.Text)) -> Blaze.p $ Blaze.toHtml t)
+
+lerParagrafosAntes :: Pool Connection -> Int -> Servant.Handler (Int, [(Int, Text)])
+lerParagrafosAntes connPool paragrafoId = fmap (paragrafoId,) $
+    withDbConnection connPool $ \conn ->
+        -- Pegamos os 2 parágrafos anteriores
+        liftIO $ query conn "WITH p AS (select conteudodiarioid, ordem from paragrafodiario where id = ?) select paragrafodiario.id, paragrafodiario.conteudo FROM paragrafodiario JOIN p USING (conteudodiarioid) WHERE paragrafodiario.ordem BETWEEN p.ordem - 2 AND p.ordem - 1 ORDER BY paragrafodiario.ordem" (Only paragrafoId)
+
+lerParagrafosDepois :: Pool Connection -> Int -> Servant.Handler (Int, [(Int, Text)])
+lerParagrafosDepois connPool paragrafoId = fmap (paragrafoId,) $
+    withDbConnection connPool $ \conn ->
+        -- Pegamos os 2 parágrafos posteriores
+        liftIO $ query conn "WITH p AS (select conteudodiarioid, ordem from paragrafodiario where id = ?) select paragrafodiario.id, paragrafodiario.conteudo FROM paragrafodiario JOIN p USING (conteudodiarioid) WHERE paragrafodiario.ordem BETWEEN p.ordem + 1 AND p.ordem + 2 ORDER BY paragrafodiario.ordem" (Only paragrafoId)
--- src/frontend/src/Main.hs
+++ src/frontend/src/Main.hs
@@ -2,6 +2,8 @@
 import Data.Proxy
 import Data.Functor ((<&>))
 import Control.Lens ((&), (%~), (.~))
+import Data.Maybe (fromMaybe, listToMaybe)
+import Data.These (These)
 import Reflex.Dom
 import Reflex.Dom.Xhr
 import Reflex.Dom.Class
@@ -16,14 +18,24 @@ import Common
 
 data Busca = NadaBuscado | Buscando (Text, Int) | BuscaTerminada ResultadoBusca
 
+data ListandoParagrafos = SemListar | Listando Int | ListaObtida (Maybe (Int, [(Int, Text)])) deriving Show
+
 search querySearchedEv = do
     responsesEv <- performRequestAsync $ toRequest <$> querySearchedEv
     let buscaIniciadaEv = Buscando <$> querySearchedEv
         resultadosChegaramEv = toBuscaTerminada <$> (decodeXhrResponse <$> responsesEv)
     holdDyn NadaBuscado (leftmost [buscaIniciadaEv, resultadosChegaramEv])
     where toRequest (termo, pagina) = xhrRequest "GET" ("/busca?q=" <> termo <> "&p=" <> tshow pagina) def
-          toBuscaTerminada Nothing = BuscaTerminada $ ErroBusca "Aconteceu um erro interno na desserialização. Por favor reporte isso como um Bug."
+          toBuscaTerminada Nothing = BuscaTerminada $ ErroBusca "Aconteceu um erro interno. Por favor atualize a página e tente novamente. Se persistir, reporte isso como um Bug."
           toBuscaTerminada (Just res) = BuscaTerminada res
+
+listarParagrafos baseUrl paragrafoListEv = do
+    responsesEv <- performRequestAsync $ toRequest <$> paragrafoListEv
+    let buscaIniciadaEv = Listando <$> paragrafoListEv
+        resultadosChegaramEv = toBuscaTerminada <$> (decodeXhrResponse <$> responsesEv)
+    holdDyn SemListar (leftmost [buscaIniciadaEv, resultadosChegaramEv])
+    where toRequest pid = xhrRequest "GET" (baseUrl <> tshow pid) def
+          toBuscaTerminada = ListaObtida
 
 main = mainWidgetWithHead htmlHead htmlBody
 
@@ -48,15 +60,22 @@ renderValor (ValorTexto t) = text t
 renderValor (ValorLista l) = el "ul" $ forM_ l $ \v -> el "li" $ renderValor v
 renderValor (ValorMatch matches) = forM_ matches $ \(s, negrito) -> if negrito then el "strong" (text s) else text s
 
+elOnClick elName className contents = do
+    (e, _) <- elClass' elName className contents
+    return $ domEvent Click e
+
 btn attrs disableEv contents = do
     allAttrs <- holdDyn attrs (fixAttrs <$> disableEv)
     elDynAttr' "button" allAttrs contents
     where fixAttrs True  = Map.insert "disabled" "" attrs
           fixAttrs False = attrs
 
-btnEv attrs disableEv contents = do
-    (b, _) <- btn attrs disableEv contents
+btnClass className disableEv contents = do
+    (b, _) <- btn (Map.fromList [("type", "button"), ("class", className)]) disableEv contents
     return $ domEvent Click b
+
+dynEv :: forall t m a b. (Reflex t, MonadHold t m, Adjustable t m, NotReady t m, PostBuild t m) => Dynamic t a -> (a -> m (Event t b)) -> m (Event t b)
+dynEv dynObj = join . fmap (switchHold never) . dyn . ffor dynObj
 
 formPreventDefault :: forall t m a. DomBuilder t m => m a -> m (Element EventResult (DomBuilderSpace m) t, a)
 formPreventDefault c =
@@ -99,7 +118,7 @@ htmlBody = do
                         el "li" $ text "Tente também \"alvará deferido data>=2019-01-01 data<=2019-01-31 grupos:data\" para ver alvarás deferidos em todos diários oficiais disponíveis por data no mês de Janeiro/2019"
                         el "li" $ text "Tente também \"covid\" para ver medidas públicas relacionadas ao Corona Vírus"
                 
-                pgClickEvEv <- dyn $ ffor resultadosDyn $ \case
+                pgClickEv :: Event DomTimeline Int <- dynEv resultadosDyn $ \case
                         NadaBuscado -> return never
                         Buscando (t, _) -> do
                             text $ "Buscando por '" <> t <> "'..."
@@ -115,16 +134,57 @@ htmlBody = do
                             elClass "table" "table table-bordered" $ do
                                 el "tr" $
                                     forM_ (colunas r) $ \col -> el "th" $ text col
-                                forM_ (resultados r) $ \(mConteudoDiarioId, res) -> el "tr" $
-                                    forM_ res $ \v -> el "td" $ do
-                                        case mConteudoDiarioId of
-                                            Just conteudoDiarioId -> elAttr "a" ("href" =: ("/ler/" <> tshow conteudoDiarioId)) $ renderValor v
+                                forM_ (resultados r) $ \(mParagrafoId, res) -> el "tr" $ do
+                                    let
+                                        primeirasColunas = take (length res - 1) res
+                                        colunaParagrafo = drop (length res - 1) res
+                                    forM_ primeirasColunas $ \v -> el "td" $ renderValor v
+                                    forM_ colunaParagrafo $ \v -> el "td" $ do
+                                        case mParagrafoId of
+                                            Just paragrafoId -> mdo
+                                                carregarAnterioresClickEv <- elClass "p" "text-center" $ btnClass "btn btn-link" never $ text "Carregar parágrafos anteriores"
+                                                paragrafosAnterioresDyn <- traceDynWith show <$> listarParagrafos "/listar-paragrafos-anteriores/" (tag (current pidMaisAntigoDyn) carregarAnterioresClickEv)
+                                                paragrafosPosterioresDyn <- listarParagrafos "/listar-paragrafos-posteriores/" (tag (current pidMaisRecenteDyn) carregarPosterioresClickEv)
+                                                pidMaisAntigoDyn <- holdDyn paragrafoId $ fforMaybe (updated paragrafosAnterioresDyn) $ \case
+                                                        SemListar -> Nothing
+                                                        Listando _ -> Nothing
+                                                        ListaObtida Nothing -> Nothing
+                                                        ListaObtida (Just (_, ps)) -> fst <$> listToMaybe ps
+                                                pidMaisRecenteDyn <- holdDyn paragrafoId $ fforMaybe (updated paragrafosPosterioresDyn) $ \case
+                                                        SemListar -> Nothing
+                                                        Listando _ -> Nothing
+                                                        ListaObtida Nothing -> Nothing
+                                                        ListaObtida (Just (_, ps)) -> fst <$> listToMaybe (reverse ps)
+                                                -- Até o merge de https://github.com/reflex-frp/reflex-dom/pull/255, precisamos verificar
+                                                -- se a Response já foi processada armazenando o paragrafoId mais recente..
+                                                todosParagrafosAnterioresDyn <- foldDynMaybe (\mpps (ultimoPid, todosPs) -> 
+                                                    case mpps of
+                                                        Nothing -> Nothing
+                                                        Just (pid, ps) ->
+                                                            if pid == ultimoPid then Nothing else Just (pid, ps ++ todosPs)) ((-1), []) $ updated paragrafosAnterioresDyn <&> \case
+                                                                SemListar -> Nothing
+                                                                Listando _ -> Nothing
+                                                                ListaObtida Nothing -> Nothing
+                                                                ListaObtida (Just (pid, ps)) -> Just (pid, fmap snd ps)
+                                                todosParagrafosPosterioresDyn <- foldDynMaybe (\mpps (ultimoPid, todosPs) -> 
+                                                    case mpps of
+                                                        Nothing -> Nothing
+                                                        Just (pid, ps) ->
+                                                            if pid == ultimoPid then Nothing else Just (pid, todosPs ++ ps)) ((-1), []) $ updated paragrafosPosterioresDyn <&> \case
+                                                                SemListar -> Nothing
+                                                                Listando _ -> Nothing
+                                                                ListaObtida Nothing -> Nothing
+                                                                ListaObtida (Just (pid, ps)) -> Just (pid, fmap snd ps)
+                                                dyn_ $ todosParagrafosAnterioresDyn <&> \(_, ps) -> forM_ ps (\t -> el "p" $ text t)
+                                                el "p" $ renderValor v
+                                                dyn_ $ todosParagrafosPosterioresDyn <&> \(_, ps) -> forM_ ps (\t -> el "p" $ text t)
+                                                carregarPosterioresClickEv <- elClass "p" "text-center" $ btnClass "btn btn-link" never $ text "Carregar parágrafos posteriores"
+                                                return ()
                                             Nothing -> renderValor v
 
                             let ultimaPagina = qtdResultados `quot` 20 + 1
                                 todasPaginasDyn = pgAtualDyn <&> \pg -> 1 : [max 2 (pg - 4) .. pg] ++ [(pg + 1) .. min (ultimaPagina - 1) (pg + 4)] ++ if pg < ultimaPagina then [ultimaPagina] else []
-                                mkPgBtn pg = (pg <$) <$> btnEv (Map.fromList [("type", "button"), ("class", "btn btn-link")]) (updated $ (== pg) <$> pgAtualDyn) (text $ tshow pg)
-                            pgClickEvEv <- elClass "div" "text-center" $ dyn $ ffor todasPaginasDyn $ \pgs -> mergeWith (const id) <$> forM pgs mkPgBtn
-                            switchHold never pgClickEvEv
-                pgClickEv <- switchHold never pgClickEvEv
+                                mkPgBtn pg = (pg <$) <$> btnClass "btn btn-link" (updated $ (== pg) <$> pgAtualDyn) (text $ tshow pg)
+                            pgClickEv <- elClass "div" "text-center" $ dynEv todasPaginasDyn $ \pgs -> mergeWith const <$> forM pgs mkPgBtn
+                            return pgClickEv
                 return ()
